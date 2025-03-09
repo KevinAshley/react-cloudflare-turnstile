0cef1dbe46117984b4cae759163fa738acc8d55a
KevinAshley <kashley3141@gmail.com>
2025-03-08 18:58:38 -0800

    working turnstile component

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,83 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
+
+declare global {
+    interface Window {
+        turnstile?: Turnstile.Turnstile;
+    }
+}
 
 export default function ReactCloudflareTurnstile({
     turnstileSiteKey,
     value,
     setValue,
-    mode,
+    theme,
 }: {
     turnstileSiteKey: string;
     value: string;
     setValue: (param: string) => void;
-    mode: "auto" | "light" | "dark";
+    theme: "auto" | "light" | "dark";
 }) {
-    return <div>test</div>;
+    const turnstileRef = useRef<HTMLDivElement>(null);
+    const [mounted, setMounted] = useState(false);
+    const turnstileIdRef = useRef<string | null | undefined>(undefined);
+    const attemptingTurnstileRef = useRef(false);
+
+    useEffect(() => {
+        // only render the turnstile after mounted is true...
+        setMounted(true);
+    }, []);
+
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = `https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit`;
+        script.async = true;
+        document.head.appendChild(script);
+    }, []);
+
+    useEffect(() => {
+        if (
+            mounted &&
+            !turnstileIdRef.current &&
+            !attemptingTurnstileRef.current
+        ) {
+            attemptingTurnstileRef.current = true;
+            (async () => {
+                while (!window.turnstile) {
+                    await new Promise((resolve) => setTimeout(resolve, 10));
+                }
+                const renderedTurnstileId = window.turnstile.render(
+                    turnstileRef.current as HTMLElement,
+                    {
+                        sitekey: turnstileSiteKey,
+                        callback: function (token) {
+                            setValue(token);
+                        },
+                        theme,
+                    }
+                );
+                turnstileIdRef.current = renderedTurnstileId;
+                attemptingTurnstileRef.current = false;
+            })();
+        }
+    }, [mounted, theme, setValue]);
+
+    useEffect(() => {
+        return () => {
+            if (turnstileIdRef.current && window.turnstile?.remove) {
+                window.turnstile.remove(turnstileIdRef.current);
+            }
+        };
+    }, []);
+
+    return (
+        <Fragment>
+            <div ref={turnstileRef} className={"react-cloudflare-turnstile"} />
+            <input
+                className={"hidden-input"}
+                value={value}
+                onChange={() => {}}
+                required={true}
+            />
+        </Fragment>
+    );
 }
