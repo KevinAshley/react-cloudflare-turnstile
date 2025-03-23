f2c6f746e621b71de5b02a3f40e41c14d1b9bb40
KevinAshley <kashley3141@gmail.com>
2025-03-22 23:45:06 -0700

    added verify function

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,32 @@
 import ReactCloudflareTurnstile from "./ReactCloudflareTurnstile";
 
+export const verifyTurnstileToken = async ({
+    cloudflareTurnstileSecretKey,
+    turnstileToken,
+}: {
+    cloudflareTurnstileSecretKey: string;
+    turnstileToken: string;
+}) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let verificationFormData = new FormData();
+            verificationFormData.append("secret", cloudflareTurnstileSecretKey);
+            verificationFormData.append("response", turnstileToken);
+            const url =
+                "https://challenges.cloudflare.com/turnstile/v0/siteverify";
+            const turnstileVerifyResponse = await fetch(url, {
+                body: verificationFormData,
+                method: "POST",
+            });
+            const turnstileVerifyOutcome = await turnstileVerifyResponse.json();
+            if (!turnstileVerifyOutcome.success) {
+                throw new Error("Invalid Cloudflare Verification");
+            }
+            resolve(true);
+        } catch (error: any) {
+            reject(error);
+        }
+    });
+};
+
 export default ReactCloudflareTurnstile;
